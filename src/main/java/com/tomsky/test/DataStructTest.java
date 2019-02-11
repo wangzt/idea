3cc4bc878f9b0978957719521992232cdf5899af
wangzhitao <wangzhitao@huajiao.tv>
2019-02-11 17:01:01 +0800

    test recycle round

--- src/main/java/com/tomsky/test/DataStructTest.java
+++ src/main/java/com/tomsky/test/DataStructTest.java
@@ -11,12 +11,13 @@ public class DataStructTest {
 
     @Test
     public void recycle() {
-        System.out.println("count:"+ f(6));
+        System.out.println("recycle count:"+ f(6));
+        System.out.println("round count:"+ round(6));
     }
 
     private int f(int n) {
         ++depth;
-        System.out.println("---"+depth);
+//        System.out.println("---"+depth);
 
         if (n == 1) return 1;
         if (n == 2) return 2;
@@ -28,6 +29,23 @@ public class DataStructTest {
 //        System.out.println("n==="+n+"---depth:"+depth);
         int ret =  f(n - 1) + f(n - 2);
         solvedMap.put(n, ret);
+
+        return ret;
+    }
+
+    private int round(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int ret = 0;
+        int pre = 2;
+        int prepre = 1;
+
+        for (int i = 3; i <= n; i++) {
+            ret = pre + prepre;
+            prepre = pre;
+            pre = ret;
+        }
 
         return ret;
     }
